4c16115d21f16d474420fe756efc92f1af6bdc61
Luca Anthony <lucamafort05@gmail.com>
2024-09-07 11:05:47 -0300

    Módulo "SpringBoot02" - Aula 05 - Vídeo 01
    + Início do módulo "Aula05SpringBoot02".

--- Sli/src/main/java/io/github/antth_Luca/api/controller/CidadeController.java
+++ Sli/src/main/java/io/github/antth_Luca/api/controller/CidadeController.java
@@ -3,11 +3,15 @@ package io.github.antth_Luca.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.antth_Luca.api.model.CidadeModel;
@@ -30,7 +34,19 @@ public class CidadeController {
     }
 
     @GetMapping("/{id}")
-    public String getById(@PathVariable String id) {
-        return "Cidade " + id;
+    public CidadeModel getById(@PathVariable Integer id) {
+        return repository.findById(id).get();  // 'findById' retorna um object 'option' que não tem os dados, por isso use com 'get';
+    }
+
+    @PutMapping
+    public CidadeModel update(@RequestBody CidadeModel cidade) {
+        return repository.save(cidade);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code=HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        CidadeModel obj = repository.findById(id).get();
+        repository.delete(obj);
     }
 }
