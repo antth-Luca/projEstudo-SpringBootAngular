7be605455722524f4b185f143d0888b8de086f3f
Luca Anthony <lucamafort05@gmail.com>
2024-09-07 00:59:15 -0300

    Aula 8
    + Finalização do módulo "Aula04SpringBoot01".

--- Sli/src/main/java/io/github/antth_Luca/api/controller/CidadeController.java
+++ Sli/src/main/java/io/github/antth_Luca/api/controller/CidadeController.java
@@ -1,16 +1,32 @@
 package io.github.antth_Luca.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.antth_Luca.api.model.CidadeModel;
+import io.github.antth_Luca.api.repository.CidadeRepository;
 
 @RestController
 @RequestMapping("/api/cidade")
 public class CidadeController {
+    @Autowired
+    private CidadeRepository repository;
+
+    @PostMapping
+    private CidadeModel inserir(@RequestBody CidadeModel cidade) {
+        return repository.save(cidade);
+    }
+
     @GetMapping
-    public String getAll() {
-        return "Lista de cidades";
+    public List<CidadeModel> getAll() {
+        return repository.findAll();
     }
 
     @GetMapping("/{id}")
---
+++ Sli/src/main/java/io/github/antth_Luca/api/repository/CidadeRepository.java
@@ -0,0 +1,9 @@
+package io.github.antth_Luca.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.antth_Luca.api.model.CidadeModel;
+
+public interface CidadeRepository extends JpaRepository<CidadeModel, Integer>{
+
+}
