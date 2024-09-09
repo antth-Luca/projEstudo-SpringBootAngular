d045dcda4df402281b4e110d38cab88f1bde6a16
Luca Anthony <lucamafort05@gmail.com>
2024-09-09 12:17:52 -0300

    Módulo "SpringBoot02" - Aula 05 - Vídeo 05
    + Fim do módulo "Módulo "SpringBoot02".

--- Sli/src/main/java/io/github/antth_Luca/api/service/ClienteService.java
+++ Sli/src/main/java/io/github/antth_Luca/api/service/ClienteService.java
@@ -1,5 +1,7 @@
 package io.github.antth_Luca.api.service;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
@@ -12,4 +14,10 @@ import jakarta.validation.Valid;
 @Service
 public interface ClienteService {
     ResponseEntity<Response<Cliente>> salvar(@Valid @RequestBody Cliente cliente, BindingResult result);
+
+    List<Cliente> listar();
+
+    ResponseEntity<Response<Cliente>> getById(Integer id);
+
+    ResponseEntity<Response<Cliente>> deleteById(Integer id);
 }
