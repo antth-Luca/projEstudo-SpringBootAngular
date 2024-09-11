606df4b23d6db000d233e500012e9130d0609eb2
Luca Anthony <lucamafort05@gmail.com>
2024-09-10 23:09:21 -0300

    Módulo "Angular01" - Aula 07 - Vídeo 04

--- front-end/Sli/src/app/app.routes.ts
+++ front-end/Sli/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { CidadeListaComponent } from './cadastros/cidade-lista/cidade-lista.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : '', redirectTo: '/home', pathMatch: 'full'},
+    { path : 'home', component: HomeComponent },
+    { path : 'login', component: LoginComponent },
+    { path : 'lista-cidades', component: CidadeListaComponent },
+];
---
+++ front-end/Sli/src/app/cadastros/cidade-lista/cidade-lista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cidade-lista',
+  standalone: true,
+  imports: [],
+  templateUrl: './cidade-lista.component.html',
+  styleUrl: './cidade-lista.component.css'
+})
+export class CidadeListaComponent {
+
+}
