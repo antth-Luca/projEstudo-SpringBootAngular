6ba2e015a79039ccea29d74ce0f6471ef462cccc
Luca Anthony <lucamafort05@gmail.com>
2024-09-11 10:02:47 -0300

    Módulo "Angular01" - Aula 07 - Vídeo 06
    * Finalização do módulo "Angular01".

--- front-end/Sli/src/app/cadastros/cidade-lista/cidade-lista.component.ts
+++ front-end/Sli/src/app/cadastros/cidade-lista/cidade-lista.component.ts
@@ -1,14 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cidade-lista',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './cidade-lista.component.html',
   styleUrl: './cidade-lista.component.css'
 })
 export class CidadeListaComponent {
-  msgSucesso: String = "Cidade registrada com sucesso!";
-  msgErro: String = "Não foi possível registrar esta cidade!";
+  msgSucesso: String = "";
+  msgErro: String = "";
 }
